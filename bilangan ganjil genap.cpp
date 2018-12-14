42cc712fa9001a2a42b3ec30fdbc80aeb4f4c788
fuadkumarudin <45659243+fuadkumarudin@users.noreply.github.com>
2018-12-14 20:10:01 +0700

    Add files via upload

---
+++ bilangan ganjil genap.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main()
+{
+	int x;
+	printf("masukan bilangan :");
+	scanf("%d" ,&x);
+	if(x%2==0)
+	{
+		printf("%d adalah bilangan genap\n\n",x);
+		
+	}
+	else{
+		printf("%d adalah bilangan ganjil\n\n",x);
+	}
+	printf ("PAUSE");
+}
+
